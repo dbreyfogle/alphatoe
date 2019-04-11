454a08a57397f26af46def351686e5e682d14838
Danny Breyfogle <DB350@ntrs.com>
2019-04-11 13:15:40 -0700

    Tuned params. Performing well

--- alphatoe.py
+++ alphatoe.py
@@ -2,8 +2,9 @@
 
 import numpy as np
 import random
-import math
 import pickle
+import sys
+import math
 
 
 class Environment():
@@ -45,7 +46,6 @@ class Environment():
 
     def get_actions(self, mark):
         """A list of actions that the player can take, i.e. empty spaces (row, col, mark)"""
-        assert mark in [1, -1], 'Invalid mark'
         actions = []
         for row in range(3):
             for col in range(3):
@@ -53,9 +53,8 @@ class Environment():
                     actions.append((row, col, mark))
         return actions
 
-    def get_rewards(self, mark):
+    def rewards(self, mark):
         """Return rewards for the player in the current state"""
-        assert mark in [1, -1], 'Invalid mark'
         goal = 3 * mark
         for i in range(3):
             row_sum = np.sum(self.state[i, :])
@@ -71,12 +70,11 @@ class Environment():
     def step(self, action):
         """Perform an action and return the new state and rewards"""
         row, col, mark = action[0], action[1], action[2]
-        assert self.state[row, col] == 0, 'Invalid space'
-        assert mark in [1, -1], 'Invalid mark'
+        assert self.state[row, col] == 0, 'Environment tried to process an invalid action'
         self.state[row, col] = mark
-        reward = self.get_rewards(mark)
-        return self.state, reward
-    
+        r = self.rewards(mark)
+        return self.state, r
+
     def is_full(self):
         """Checks if the board is full"""
         for row in range(3):
@@ -87,7 +85,7 @@ class Environment():
 
     def is_done(self):
         """Check if the game is finished"""
-        return self.get_rewards(1) or self.is_full()
+        return self.rewards(1) or self.is_full()
 
 
 class Agent():
@@ -95,6 +93,7 @@ class Agent():
 
     def __init__(self, mark):
         """Setup an agent and assign it X or O"""
+        assert mark in [-1, 1], 'Mark must be +/- 1'
         self.Q = {} # States, actions, q-values
         self.m = mark
         self.s = np.zeros((3, 3))
@@ -108,12 +107,12 @@ class Agent():
         self.a = None
 
     def save_model(self, path):
-        """Save the model to a file at the chosen path"""
+        """Save the model to a pickle file at the path"""
         with open(path, 'wb') as fp:
             pickle.dump(self.Q, fp, protocol=pickle.HIGHEST_PROTOCOL)
 
     def load_model(self, path):
-        """Load a model from a file at the path"""
+        """Load a model from a pickle file at the path"""
         with open(path, 'rb') as fp:
             self.Q = pickle.load(fp)
 
@@ -123,6 +122,7 @@ class Agent():
 
     def set_mark(self, mark):
         """Set X or O"""
+        assert mark in [-1, 1], 'Mark must be +/- 1'
         self.m = mark
 
     def evaluate(self, state=None):
@@ -130,18 +130,17 @@ class Agent():
         if state is None: state = self.s
         s = tuple(state.flatten())
         try: Q_s = {a: q for a, q in self.Q[s].items() if a[2] == self.m}
-        except: Q_s = {} # Not seen
+        except: Q_s = {} # No history
         return Q_s
 
-    def act(self, env, e=0.2):
+    def act(self, env, e=0.3):
         """Take an action in the Environment and return rewards. Random actions are taken e percent of the time"""
         self.s[:] = env.get_state()
         actions = env.get_actions(self.m)
         a = random.choice(actions) # Explore
         if e < random.uniform(0, 1):
             Q_s = self.evaluate()
-            if Q_s and max(Q_s.values()) > 0:
-                a = max(Q_s, key=Q_s.get) # Enhance
+            if Q_s: a = max(Q_s, key=Q_s.get) # Enhance
         s1, r = env.step(a)
         s = tuple(self.s.flatten())
         if s not in self.Q: self.Q[s] = {}
@@ -149,8 +148,9 @@ class Agent():
         self.s_prev[:], self.s[:], self.a = self.s, s1, a
         return r
 
-    def observe(self, r, lr=0.3, y=0.5):
-        """Observe rewards and update Q for the previous state and action"""
+    def observe(self, r, lr=0.2, y=0.5):
+        """Observe rewards and update Q. The learning rate determines how heavily recent observations
+        impact the model. The discount factor (y) diminishes the value of long term rewards"""
         if self.a is None: return # Nothing to observe
         s = tuple(self.s_prev.flatten())
         q, q1 = self.Q[s][self.a], 0
@@ -164,111 +164,92 @@ class Human():
 
     def __init__(self, mark):
         """Assign X/O"""
+        assert mark in [-1, 1], 'Mark must be +/- 1'
         self.m = mark
 
     def act(self, env):
         """Ask for the next move and update the Environment"""
         env.display()
-        print('Your turn:')
-        valid = False
-        while not valid:
-            row = int(input('Row #: '))
-            col = int(input('Column #: '))
+        r = None
+        while True:
+            row = raw_input('Row #: ')
+            if row == 'q': sys.exit()
+            col = raw_input('Column #: ')
+            if col == 'q': sys.exit()
             try:
-                if env.get_state()[row, col] == 0: # Must exist and be empty
-                    valid = True
-            except:
-                print('Sorry, that spot is unavailable. Try again:')
-        _, r = env.step((row, col, self.m))
+                _, r = env.step((int(row), int(col), self.m))
+                break
+            except: print("Sorry, try again. ('q' to exit)")
         return r
 
 
 if __name__ == '__main__':
-    
-    i = 5000
-    print('lr', 0.7 * math.exp(-1.5e-4 * i))
-    print('e', 0.3 * math.exp(-9e-5 * i))
-    
-    n_games = 20000
+
+    # Training
     env = Environment()
     a1 = Agent(1)
     a2 = Agent(-1)
+    n_games = 100000
+    e = 0.3 # Epsilon
+    y = 0.5 # Gamma
     a1_wins, a2_wins = 0, 0
     for i in range(n_games):
-        if (i + 1) % 2000 == 0: print('On run ' + str(i))
-        lr = 0.7 * math.exp(-1.5e-4 * i)
-        e = 0.3 * math.exp(-9e-5 * i)
-        if a1_wins <= a2_wins:
-            while True:
-                a1.observe(a1.act(env, e), lr)
-                a2.observe(env.get_rewards(a2.get_mark()), lr)
-                if env.is_done():
-                    if env.get_rewards(1): a1_wins += 1
-                    env.reset()
-                    a1.reset()
-                    a2.reset()
-                    break
-                a2.observe(a2.act(env, e), lr)
-                a1.observe(env.get_rewards(a1.get_mark()), lr)
-                if env.is_done():
-                    if env.get_rewards(1): a2_wins += 1
-                    env.reset()
-                    a1.reset()
-                    a2.reset()
-                    break
-        elif a1_wins > a2_wins:
-            while True:
-                a2.observe(a2.act(env, e), lr)
-                a1.observe(env.get_rewards(a1.get_mark()), lr)
-                if env.is_done():
-                    if env.get_rewards(1): a2_wins += 1
-                    env.reset()
-                    a1.reset()
-                    a2.reset()
-                    break
-                a1.observe(a1.act(env, e), lr)
-                a2.observe(env.get_rewards(a2.get_mark()), lr)
-                if env.is_done():
-                    if env.get_rewards(1): a1_wins += 1
-                    env.reset()
-                    a1.reset()
-                    a2.reset()
-                    break
-    print('a1 wins: {}, a2 wins: {}, ties: {}'.format(a1_wins, a2_wins, n_games - (a1_wins + a2_wins)))
-    
+        if i % 10000 == 0: print('Training run {} of {}'.format(i, n_games))
+        lr = 0.7 * math.exp(-1.5e-5 * i) # Learning rate (decaying)
+        while True:
+            r = a1.act(env, e)
+            a1.observe(r, lr, y)
+            a2.observe(-r, lr, y)
+            if env.is_done():
+                a1_wins += r
+                env.reset()
+                a1.reset()
+                a2.reset()
+                break
+            r = a2.act(env, e)
+            a2.observe(r, lr, y)
+            a1.observe(-r, lr, y)
+            if env.is_done():
+                a2_wins += r
+                env.reset()
+                a1.reset()
+                a2.reset()
+                break
+    print('a1 wins: {}, a2 wins: {}, Ties: {}'.format(a1_wins, a2_wins, n_games - (a1_wins + a2_wins)))
     a1.save_model('model.pkl')
+
+    # Interactive
+    env = Environment()
+    you = Human(-1)
     atoe = Agent(1)
     atoe.load_model('model.pkl')
-    
     n_games = 3
-    env = Environment()
-    me = Human(-1)
+    print('Best of {}. Good luck!'.format(n_games))
+    e = 0 # Epsilon
+    lr = 0.2 # Learning rate
+    y = 0.5 # Gamma
     atoe_wins, your_wins = 0, 0
     for i in range(n_games):
         while True:
-            atoe.observe(atoe.act(env, e=0), lr=0.2)
+            r = atoe.act(env, e)
+            print('Q-values:', sorted(atoe.evaluate(atoe.s_prev).items()))
+            atoe.observe(r, lr)
             if env.is_done():
                 env.display()
-                if env.get_rewards(1):
-                    print('AlphaToe won')
-                    atoe_wins += 1
-                else: print('Tie game')
+                if r: print('AlphaToe won!')
+                else: print('Tie game.')
+                atoe_wins += r
                 env.reset()
                 atoe.reset()
                 break
-            atoe.observe(-me.act(env), lr=0.2)
+            r = you.act(env)
+            atoe.observe(r, lr)
             if env.is_done():
                 env.display()
-                if env.get_rewards(1):
-                    print('You won')
-                    your_wins += 1
-                else: print('Tie game')
+                if r: print('You won!')
+                else: print('Tie game.')
+                your_wins += r
                 env.reset()
                 atoe.reset()
                 break
-    print('Your wins: {}, alphatoe wins: {}, ties: {}'.format(your_wins, atoe_wins, (n_games - (atoe_wins + your_wins))))
-    
-    state = np.array((-1, 0, 1, \
-                      -1, -1, 1, \
-                      1, 0, 0)).reshape((3,3))
-    atoe.evaluate(state)
+    print('Your wins: {}, AlphaToe wins: {}, Ties: {}'.format(your_wins, atoe_wins, (n_games - (atoe_wins + your_wins))))
